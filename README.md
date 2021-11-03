3c8ff3610b09ab7130b002e3a1a1be0185dd5d11
Ankur Bhatkalkar <ab7869@nyu.edu>
2021-11-02 22:12:08 -0400

    Interactivity using DOM manipulation

--- README.md
+++ README.md
@@ -7,3 +7,13 @@
 2. Functions
 3. Variables
 4. Console Interaction (I/O)
+
+
+## Part 2 : Develop an interactive version using DOM manipulation.
+
+### Topics covered
+
+1. Document Object Model.
+2. Event listeners and handlers.
+3. Git Fork and Branch methodology.
+
--- index.js
+++ index.js
@@ -1,44 +1,97 @@
-alert("Welcome to Rock Paper Scissors, please have your devtools open if you are using Chrome");
+// alert("Welcome to Rock Paper Scissors, please have your devtools open if you are using Chrome");
 
-let name = prompt("Enter your name");
+// let name = prompt("Enter your name");
 
-alert("Hello ! "+name);
+// alert("Hello ! "+name);
 
 let playerScore = 0;
 let computerScore = 0;
 let plays = ["ROCK","PAPER","SCISSOR"]; 
+const player = document.getElementById("playerScore");
+const computer = document.getElementById("computerScore");
+const roundWinner = document.getElementById("roundWinner");
+const winner = document.getElementById("winner");
+const buttons = document.querySelectorAll('button');
+const roundPlay = document.querySelector('.roundPlay');
+const roundOutcome = document.querySelector('#roundOutcome');
+const whoWon = document.querySelector('#whoWon');
+const playerScoreDiv = document.querySelector('.playerScoreDiv');
+const computerScoreDiv = document.querySelector('.computerScoreDiv');
 const generatePlay = () => {
     return plays[Math.floor(Math.random()*3)];
 }
 const initiateRound = (player,computer) =>{
-    console.log(`Player used ${player} Computer used ${computer}`);
+    roundPlay.style.display = "block";
+    roundOutcome.textContent = `Player used ${player} Computer used ${computer}`;
     if(player === computer){
-        console.log("This round is a draw!")
+        whoWon.textContent = "This round is a draw!";
+        playerScoreDiv.classList.remove('win');
+        computerScoreDiv.classList.remove('win');
     }
     else{
         playerWin = 1 ? (player === "ROCK" && computer === "SCISSOR") || (player === "SCISSOR" && computer === "PAPER") || (player==="PAPER" && computer === "ROCK") : 0;
         computerWin= 1 ? !((player === "ROCK" && computer === "SCISSOR") || (player === "SCISSOR" && computer === "PAPER") || (player==="PAPER" && computer === "ROCK")) : 0;
         if(playerWin && !computerWin){
             playerScore+=1;
-            console.log("Player wins this round!");
+            whoWon.textContent = "Player wins this round!";
+            playerScoreDiv.classList.add('win');
+            computerScoreDiv.classList.remove('win');
         }else{
             computerScore+=1;
-            console.log("Computer wins this round!");
+            whoWon.textContent = "Computer wins this round!";
+            computerScoreDiv.classList.add('win');
+            playerScoreDiv.classList.remove('win');
+            // console.log("Computer wins this round!");
         }
-        console.log(`Current score is player :${playerScore} , Computer : ${computerScore}`);
+        // console.log(`Current score is player :${playerScore} , Computer : ${computerScore}`);
     }
 }
 const checkWinner = () => {
-    let winner = playerScore === computerScore ? "It's a Draw!" : playerScore > computerScore ? "Player Wins !" : "Computer Wins !";
-    console.log(winner);
-}
-for(let i = 0; i<5;i++){
-    let input = parseInt(prompt("Enter 1 for Rock, 2 for paper and 3 for scissors")); 
-    if (input && input <4 && input > 0){
-        initiateRound(plays[input-1],generatePlay());
-    }else
-    {
-        console.log("Invalid input: this round will be skipped");
+    if (playerScore === 5 || computerScore === 5){
+        let finalWinner = playerScore >  computerScore ? "Player" : "Computer";
+        roundWinner.style.display = "block";
+        winner.textContent = `${finalWinner} wins the game, hit restart for a new round!`;
+        buttons.forEach(button => {if(button.value !== "restart") console.log(button.disabled = true)});
     }
 }
-checkWinner();
+
+const restartRound= () => {
+    buttons.forEach(button=> button.disabled = false);
+    playerScore = 0;
+    computerScore = 0;
+    player.textContent = playerScore;
+    computer.textContent = computerScore;
+    roundWinner.style.display = "none";
+    roundPlay.style.display = "none";
+    playerScoreDiv.classList.remove('win');
+    computerScoreDiv.classList.remove('win');
+}
+// Commented out 5 round playing logic
+// for(let i = 0; i<5;i++){
+//     let input = parseInt(prompt("Enter 1 for Rock, 2 for paper and 3 for scissors")); 
+//     if (input && input <4 && input > 0){
+//         initiateRound(plays[input-1],generatePlay());
+//     }else
+//     {
+//         console.log("Invalid input: this round will be skipped");
+//     }
+// }
+// checkWinner();
+
+function updateScores(){
+    player.textContent = playerScore;
+    computer.textContent = computerScore;
+}
+
+function play(e){
+    if(this.value==="restart"){
+        restartRound();
+        return;
+    }
+
+    initiateRound(this.value,generatePlay());
+    updateScores();
+    checkWinner();
+}
+
+buttons.forEach(button => button.addEventListener('click',play))
